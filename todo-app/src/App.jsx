e55e6b903cebf6d4c5ee9b4290a59ebe1c666f45
mandar wajage <mandarwajage@gmail.com>
2024-01-13 10:00:05 +0530

    adding new components

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,48 +1,16 @@
 import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
+import TodoItem1 from "./components/TodoItem1";
+import TodoItem2 from "./components/TodoItem2";
 
 function App() {
   return (
     <center className="todo-container">
       <AppName/>
-      <div class="container text-center">
-        <div class="row">
-          <div class="col-6">
-            <input type="text" placeholder="Enter Todo here"></input>
-          </div>
-          <div class="col-4">
-            <input type="date"></input>
-          </div>
-          <div class="col-2">
-            <button type="button" class="btn btn-success">
-              Add
-            </button>
-          </div>
-        </div>
+      <AddTodo/>
+      <TodoItem1/>
+      <TodoItem2/>
 
-        <div class="row">
-          <div class="col-6">By Milk</div>
-          <div class="col-4">8/01/2024</div>
-          <div class="col-2">
-            <button type="button" class="btn btn-danger">
-              Danger
-            </button>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-6">
-            Karu 420
-          </div>
-          <div class="col-4">
-            1/01/2024
-          </div>
-          <div class="col-2">
-          <button type="button" class="btn btn-danger">
-              Danger
-            </button>
-          </div>
-        </div>
-      </div>
     </center>
   );
 }
